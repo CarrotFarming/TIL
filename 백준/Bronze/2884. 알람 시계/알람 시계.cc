1ac5c4e53119cac28bffa68985c400e953e69bf2
KJW <71117552+CarrotFarming@users.noreply.github.com>
2023-05-04 10:15:41 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.cc
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -6,8 +6,8 @@ int main(){
     cin >> h >> m;
     
     if(m >= 45) cout << h << ' ' << m-45;
-    else if(h == 0) cout << 23 << ' ' << 60-(45-m);
-    else cout << h-1 << ' ' << 60-(45-m);
+    else if(h == 0) cout << 23 << ' ' << 15+m;
+    else cout << h-1 << ' ' << 15+m;
     
     return 0;
 }
