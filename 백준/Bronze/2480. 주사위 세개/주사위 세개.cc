2f6b13419d3e41ec5efc8bbdf9db915c29ff4cd7
KJW <71117552+CarrotFarming@users.noreply.github.com>
2023-05-06 05:11:23 +0900

    Update 주사위 세개.cc

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -2,21 +2,27 @@
 using namespace std;
 
 int main(){
-    int a, b, c;
+    int num[3];
     int max, tmp;
-    cin >> a >> b >> c;
+    cin >> num[0] >> num[1] >> num[2];
   
-    if(a == b && b == c && a == c) cout << 10000 + a*1000;
-    else if(a != b && b != c && a != c){
-        max = a;
-        if(a < b){
-            if(b < c) max = c;
-            else max = b;
-        }else if(a < c) max = c;
+    // 모두 같은 눈이 나온 경우
+    if(num[0] == num[1] && num[1] == num[2] && num[0] == num[2])
+        cout << 10000 + num[0]*1000;
+    
+    // 모두 다른 눈이 나온 경우
+    else if(num[0] != num[1] && num[1] != num[2] && num[0] != num[2]){
+        max = num[0];
+        for(int i=1; i<3; i++){
+        if(max<num[i]) max = num[i];
+        }
         cout << max*100;
-    }else{
-        if(a == b || a == c) tmp = a;
-        else tmp = b;
+    }
+   
+    // 같은 눈이 2개 나온 경우
+    else{
+        if(num[0] == num[1] || num[0] == num[2]) tmp = num[0];
+        else tmp = num[1];
         cout << 1000 + tmp*100;
     }
     return 0;
