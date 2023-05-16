b84bf5bab5f66f87f1bb46ed54355435e350834e
KJW <71117552+CarrotFarming@users.noreply.github.com>
2023-05-17 06:27:34 +0900

    [Bronze II] Title: 단어의 개수, Time: 40 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -6,14 +6,11 @@ int main(){
 	string str;
     getline(cin, str);
     
-    if(str.empty()) cout << '0';
-    else {
-        for(int i=0; i<str.length(); i++){
-            if(str[i] == ' ') cnt++;
-        }
-        if(str[0] == ' ') cnt--;
-        if(str[str.length()-1] == ' ') cnt--;
+    for(int i=0; i<str.length(); i++){
+        if(str[i] == ' ') cnt++;
     }
+    if(str[0] == ' ') cnt--;
+    if(str[str.length()-1] == ' ') cnt--;
 
     cout << cnt;
     
