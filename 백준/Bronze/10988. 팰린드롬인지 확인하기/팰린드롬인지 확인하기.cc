ebaf9b4339e3ff3331e4b02ef4b13c1c283be472
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-05-21 12:31:40 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -3,7 +3,6 @@ using namespace std;
 
 int main(){
 	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
-	cin.tie(NULL);	// 입력과 출력 묶음을 풀기
     
     bool check = 1;
     string str;
@@ -13,12 +12,11 @@ int main(){
     for(int i=0; i <len/2; i++){
         if(str[i] != str[len-1-i]) {
             check = 0;
+            cout << '0';
             break;
         }
     }
-
     if(check) cout << '1';
-    else cout << '0';
 
     return 0;
 }
