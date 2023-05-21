78a2edf929519e11ba57033c59a010d19b1a6f00
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-05-21 12:39:28 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
 
+bool isPalindrome(string s){
+    int len = s.length();
+    for(int i=0; i < len/2; i++){
+        if(s[i] != s[len-1-i]) {
+            return 0;
+        }       
+    }
+    return 1;
+}
+
 int main(){
-	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
-    
-    bool check = 1;
+    ios_base::sync_with_stdio(false);   // 두 표준 입출력 동기화 해제
+
     string str;
     cin >> str;
-    int len = str.length();
-
-    for(int i=0; i <len/2; i++){
-        if(str[i] != str[len-1-i]) {
-            check = 0;
-            cout << '0';
-            break;
-        }
-    }
-    if(check) cout << '1';
+    cout << isPalindrome(str);
 
     return 0;
 }
