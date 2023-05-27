ecb72ede95cbfd58ee8b95d9ab8b2b9d411a5635
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-05-27 19:03:01 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub
    단계별 풀이 과정에 맞춰 2차원 배열로 풀이

--- 백준/Bronze/10798. 세로읽기/세로읽기.cc
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+    ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
 
     string matrix[5][15];
     for(int i=0; i<5; i++){
