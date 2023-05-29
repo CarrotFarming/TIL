8ce49b3150e9b5a7a5de0898ff043d6576f3941a
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-05-30 01:04:26 +0900

    [Bronze I] Title: 진법 변환 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+    
+    int n, b;
+    string res;
+    cin >> n >> b;
+
+    while(n > 0){
+        int tmp = n % b;
+        if(0 <= tmp && tmp <= 9) res += (tmp+'0');
+        else res += (tmp-10 + 'A');
+        n /= b;
+    }
+    
+    for(int i=res.length()-1; i>=0; i--){
+        cout << res[i];
+    }
+    return 0;
+}
