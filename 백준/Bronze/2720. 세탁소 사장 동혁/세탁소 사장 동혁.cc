85b4532e9549b929a1bf76749cabb369de295f00
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-05-31 00:03:09 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main(){
-	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
-	cin.tie(NULL);	// 입력과 출력 묶음을 풀기
+    ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+    cin.tie(NULL);	// 입력과 출력 묶음을 풀기
 
     int t, c;
     int coin[4] = {25, 10, 5, 1};
@@ -17,6 +17,5 @@ int main(){
         }
         cout << '\n';
     }
-    
     return 0;
 }
