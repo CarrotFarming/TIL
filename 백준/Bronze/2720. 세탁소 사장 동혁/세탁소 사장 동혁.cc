190f12df233a3e6ed0df943346d09b5f63190a94
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-05-31 00:00:37 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.cc
@@ -1,27 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int t;
+int main(){
+	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+	cin.tie(NULL);	// 입력과 출력 묶음을 풀기
+
+    int t, c;
+    int coin[4] = {25, 10, 5, 1};
     cin >> t;
 
-    while (t--) {
-        int c;
+    while(t--){
         cin >> c;
-
-        int quarter = c / 25;
-        c %= 25;
-
-        int dime = c / 10;
-        c %= 10;
-
-        int nickel = c / 5;
-        c %= 5;
-
-        int penny = c;
-
-        cout << quarter << ' ' << dime << ' ' << nickel << ' ' << penny << '\n';
+        for(int i=0; i<4; i++){
+            cout << c/coin[i] << ' ';
+            c %= coin[i];
+        }
+        cout << '\n';
     }
-
+    
     return 0;
 }
