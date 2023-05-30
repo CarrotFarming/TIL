6d79ca8aab7fc608c0f7e17fdd6cc3170b55c5ff
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-05-31 02:03:00 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.cc
+++ 백준/Bronze/2745. 진법 변환/진법 변환.cc
@@ -1,21 +1,19 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-int main(){
-    ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+int main() {
+    ios_base::sync_with_stdio(false); // 두 표준 입출력 동기화 해제
 
-    int b, tmp;
+    int b;
     int res = 0;
     string n;
     cin >> n >> b;
 
-    for(int i=0; i<n.length(); i++){
-        if('0' <= n[i] && n[i] <= '9') tmp = n[i] - '0';
-        else tmp = n[i] - 'A' + 10;
-        res += tmp * pow(b, n.length() - i - 1);
+    for (int i = 0; i < n.length(); i++) {
+        if ('0' <= n[i] && n[i] <= '9') res = res * b + (n[i] - '0');
+        else res = res * b + (n[i] - 'A' + 10);
     }
-    
+
     cout << res;
     return 0;
 }
