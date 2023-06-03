3806bca30f08cd30bc31bc7c19677bafe673e095
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-06-03 15:39:02 +0900

    [지원] [오후 3:34] [Bronze V] Title: 큰 수 A+B, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.cc
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.cc
@@ -1,38 +1,39 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-
 using namespace std;
 
-string addBigNumbers(string num1, string num2) {
-    string result;
+string addNum(string a, string b) {
+    string res;
 
-    int maxLen = max(num1.length(), num2.length());
-    num1 = string(maxLen - num1.length(), '0') + num1;
-    num2 = string(maxLen - num2.length(), '0') + num2;
+    // 큰 수로 자릿수 맞추기
+    int maxLen = max(a.length(), b.length());
+    a = string(maxLen - a.length(), '0') + a;
+    b = string(maxLen - b.length(), '0') + b;
 
-    int carry = 0;
+    int carry = 0; // 올림
     for (int i = maxLen - 1; i >= 0; i--) {
-        int digitSum = (num1[i] - '0') + (num2[i] - '0') + carry;
-        carry = digitSum / 10;
-        result += to_string(digitSum % 10);
+        int digitSum = (a[i] - '0') + (b[i] - '0') + carry; // 각 자릿수 계산
+        carry = digitSum / 10; // 올림 계산
+        res += (digitSum % 10) + '0'; // 자리수를 결과에 더함
     }
 
-    if (carry > 0) {
-        result += to_string(carry);
-    }
+    // 연산을 모두 마친 후에 올림 값이 남아있다면 올림 값을 결과에 더해줌
+    if (carry > 0) res += carry + '0';
 
-    reverse(result.begin(), result.end());
+    // 결과를 뒤집어 출력하면 원하는 연산 결과를 얻을 수 있음
+    reverse(res.begin(), res.end());
 
-    return result;
+    return res;
 }
 
 int main() {
-    string num1, num2;
-    cin >> num1 >> num2;
-
-    string sum = addBigNumbers(num1, num2);
-    cout << sum << endl;
-
+    /* ios_base::sync_with_stdio(false): 입출력 동기화가 해제되므로 입출력 성능이 향상 - 대량의 입력 또는 출력 작업이 필요한 경우에 유용
+    하지만 사용하는 메모리 버퍼 크기가 증가해 메모리 사용량이 늘어날 수 있음 (특히 대량의 입력을 처리할 때 큰 영향)
+    > 따라서 코드 사용 시 성능은 향상되지만 메모리 사용량이 늘어날 수 있으므로 상황에 맞게 사용해야함
+    > 해당 코드에서는 이 코드를 사용 시 2168KB의 메모리 사용, 사용하지 않을 때(2024KB)와 약간의 차이를 보임 */
+    string a, b;
+    cin >> a >> b;
+    cout << addNum(a, b);
     return 0;
 }
