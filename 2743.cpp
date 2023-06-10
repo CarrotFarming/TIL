b97cedb92d0e6044d270ae51a298063df4b471b0
CarrotFarming <livelim313@gmail.com>
2023-06-10 22:00:01 +0900

    test commit

---
+++ 2743.cpp
@@ -0,0 +1,13 @@
+/* DATE: 2023.05.15
+   NO: 2743
+   TITLE: 단어 길이 재기 */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    cout << str.length();
+    return 0;
+}
