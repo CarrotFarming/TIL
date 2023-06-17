d41f5c9948ee2c6f2129c17f5871cda05cf7a2ad
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-06-18 07:37:23 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int decomposition(int n);
+
+int main(){
+	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+	cin.tie(NULL);	// 입력과 출력 묶음을 풀기
+    
+    int n;
+    cin >> n;
+    int m = 1;
+    bool print = 0;
+
+    while(m < n){
+        if(n == m + decomposition(m)){
+            print = 1;
+            cout << m;
+            break;
+        } else m++;
+    }
+    if(!print) cout << 0;
+    return 0;
+}
+
+int decomposition(int n){
+    int sum = 0;
+    while(n > 0){
+        sum += n%10;
+        n /= 10;
+    }
+    return sum;
+}
