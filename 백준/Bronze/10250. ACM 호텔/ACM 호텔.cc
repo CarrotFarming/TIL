dc16a81244c7042b8e4bfcc1d9185a3f62485755
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-06-18 02:25:36 +0900

    [Bronze III] Title: ACM 호텔, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+	cin.tie(NULL);	// 입력과 출력 묶음을 풀기
+    int testCase, h, w, n, roomNum;
+    cin >> testCase;
+    while(testCase--){
+        cin >> h >> w >> n;
+        int num = 1;
+        while(n > h){
+            n -= h;
+            if(num <= w) num++;
+            else num = 1;
+        }
+        cout << n*100 + num << '\n';
+    }
+    return 0;
+}
