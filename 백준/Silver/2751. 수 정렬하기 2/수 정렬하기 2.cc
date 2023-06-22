be80617a18477fba6be670fc1e1f3e4801b1d2eb
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-06-22 16:38:08 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 272 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+	cin.tie(NULL);	// 입력과 출력 묶음을 풀기
+
+    int n;
+    cin >> n;
+    int num[n];
+    for(int i=0; i<n; i++){
+        cin >> num[i];
+    }
+    sort(num, num+n);
+    for(int i=0; i<n; i++){
+        cout << num[i] << '\n';
+    }
+    return 0;
+}
