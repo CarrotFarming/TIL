e04810b5d10646886fb7e99999e68955e18b30a0
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-06-25 22:14:37 +0900

    [Silver V] Title: 나이순 정렬, Time: 48 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+using namespace std;
+
+bool sorting(tuple<int, int, string> &v1, tuple<int, int, string> &v2);
+
+int main(){
+	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+	cin.tie(NULL);	// 입력과 출력 묶음을 풀기
+
+    int n, age;
+    string name;
+    cin >> n;
+    
+    vector<tuple<int, int, string>> list;
+    for(int i=0; i<n; i++){
+        cin >> age >> name;
+        list.push_back(make_tuple(i, age, name));
+    }
+    
+    sort(list.begin(), list.end(), sorting);
+    for(int i=0; i<n; i++){
+        cout << get<1>(list[i]) << ' ' << get<2>(list[i]) << '\n';
+    }
+    return 0;
+}
+
+bool sorting(tuple<int, int, string> &v1, tuple<int, int, string> &v2){
+    if(get<1>(v1) == get<1>(v2)) return get<0>(v1) < get<0>(v2);
+    else return get<1>(v1) < get<1>(v2);
+}
