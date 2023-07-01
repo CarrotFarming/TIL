c102bc81bafa17c7fc1acb6c7873026927d2d777
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-07-01 19:13:57 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 3852 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,24 +1,26 @@
 #include <iostream>
+#include <array>
 #include <algorithm>
 using namespace std;
 
-int arr[500002];
-int n, m, card;
+int main(){
+	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+	cin.tie(NULL);	// 입력과 출력 묶음을 풀기
+    
+    int n, m, card;
+    array<int, 500001> cardSet;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> card;
+        cardSet[i] = card;
+    }
+    
+    sort(cardSet.begin(), cardSet.begin()+n); // 이진탐색을 위해 정렬
+    cin >> m;
+    for(int i=0; i<m; i++){
+        cin >> card;
+		cout << upper_bound(cardSet.begin(), cardSet.begin()+n, card) - lower_bound(cardSet.begin(), cardSet.begin()+n, card)<< ' ';
+    }
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cin >> n;
-	for (int i=0; i<n; i++) {
-		cin >> card;
-		arr[i] = card;
-	}
-	sort(arr, arr+n); // 이분탐색을 위해 오름차순 정렬
-
-	cin >> m;
-	for (int i=0; i<m; i++) {
-		cin >> card;
-		cout << upper_bound(arr, arr+n, card) - lower_bound(arr, arr+n, card)<<" ";
-	}
     return 0;
 }
