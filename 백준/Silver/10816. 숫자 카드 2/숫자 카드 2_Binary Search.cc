34532055d28d86c1bcea0d1b40d1a66e66530bac
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-07-02 11:08:19 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 312 ms, Memory: 3852 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2_Binary Search.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2_Binary Search.cc
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main(){
-	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
-	cin.tie(NULL);	// 입력과 출력 묶음을 풀기
+    ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+    cin.tie(NULL);	// 입력과 출력 묶음을 풀기
     
     int n, m, card;
     array<int, 500001> cardSet;
