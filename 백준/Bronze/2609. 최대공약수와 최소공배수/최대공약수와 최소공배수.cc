d03bc33d0801dd36ec6f77755bceda6c252711b8
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-07-06 21:30:30 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,22 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int a, int b) {
-	int c = a % b;
-	while (c != 0) {
-		a = b;
-		b = c;
-		c = a % b;
-	}
-	return b;
-}
+int main(){
+    int a, b, r;
+    cin >> a >> b;
+    int tmpA = a;
+    int tmpB = b;
+	
+    int GCD, LCM;
+    while(true){
+        r = a%b;
+        if(r == 0){
+            GCD = b;
+            break;
+        }
+        a = b;
+        b = r;
+    }
+    LCM = tmpA*tmpB / GCD;
 
-int lcm(int a, int b) {
-	return (a * b) / gcd(a, b);
-}
-
-int main() {
-	int n1, n2;
-	cin >> n1 >> n2;
-	cout << gcd(n1, n2) << "\n" << lcm(n1, n2);
+    cout << GCD << '\n' << LCM;
+    return 0;
 }
