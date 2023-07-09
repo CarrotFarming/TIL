f8368b802d7c6bda1e290f1147fb82667caf85f2
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-07-09 17:45:38 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -5,8 +5,8 @@ int main(){
     int a, b;
     cin >> a >> b;
     
-    if(a>b) cout << '>';
-    else if(a<b) cout << '<';
+    if(a > b) cout << '>';
+    else if(a < b) cout << '<';
     else cout << "==";
     
     return 0;
