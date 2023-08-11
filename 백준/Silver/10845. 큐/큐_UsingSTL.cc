cff98fd46d0c308239452f3164432889899db0f5
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-08-11 15:47:33 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐_UsingSTL.cc
+++ 백준/Silver/10845. 큐/큐_UsingSTL.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+    ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
     cin.tie(NULL);	// 입력과 출력 묶음을 풀기
     
     int n, num;
