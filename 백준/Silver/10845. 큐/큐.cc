b237db2766488b4a29c3c259df80b8135633f7ea
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-08-11 15:45:47 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);	// 두 표준 입출력 동기화 해제
+    cin.tie(NULL);	// 입력과 출력 묶음을 풀기
+    
+    int n, num;
+    cin >> n;
+    queue<int> q;
+    
+    while(n--){
+        string s;
+        cin >> s;
+        
+        if(s == "push"){
+            cin >> num;
+            q.push(num);
+        }
+        else if(s == "pop"){
+            if(q.empty()) cout << -1 << '\n';
+            else{
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if(s == "size"){
+            cout << q.size() << '\n';
+        }
+        else if(s == "empty"){
+            cout << q.empty() << '\n';
+        }
+        else if(s == "front"){
+            if(q.empty()) cout << -1 << '\n';
+            else cout << q.front() << '\n';
+        }
+        else if(s == "back"){
+            if(q.empty()) cout << -1 << '\n';
+            else cout << q.back() << '\n';
+        }
+        else cout << "error!\n";
+    }
+    
+    return 0;
+}
