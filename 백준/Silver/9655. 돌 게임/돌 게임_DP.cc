7751545be999ee8743f11e65410e09bb19520255
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-11-07 16:08:59 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임_DP.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임_DP.cc
@@ -10,9 +10,9 @@ int main(){
     int D[MAX]; // DP 테이블 생성 및 초기값 할당
     D[1] = 1;
     D[2] = 2;
-    D[3] = 3;
+    D[3] = 1;
 
-    for(int i = 4; i < MAX; i++){
+    for(int i = 4; i < n; i++){
         D[i] = min(D[i-1] + 1, D[i-3] + 1);
     }
 
