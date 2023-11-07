f4f152431d5d885d1ccda9052bc76cf77d7791e4
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-11-07 10:01:48 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.cc
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -1,19 +1,12 @@
 #include <iostream>
 using namespace std;
 
-bool turn(int n);
-
 int main(){
     int n;
     cin >> n;
     
-    if(turn(n)) cout << "SK";
-    else cout << "CY";
+    if(n%2 == 0) cout << "CY";
+    else cout << "SK";
     
     return 0;
-}
-
-bool turn(int n){
-    if(n!=1) return !turn(n-1);
-    else return 1;
 }
