50253c57b3e32327a6adaee88ab1c1389fb413a1
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-11-07 17:39:49 +0900

    [Silver I] Title: 정수 삼각형, Time: 40 ms, Memory: 3860 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -6,14 +6,14 @@ int main(){
     int n;
     cin >> n;
 
-    int input[501][501]; // 정수 삼각형
+    int input[502][502]; // 정수 삼각형
     for(int i = 1; i < n+1; i++){ // line
         for(int j = 0; j < i; j++){ // index
             cin >> input[i][j];
         }
     }
 
-    int D[501][501]; // DP 테이블 생성 및 초기값 할당
+    int D[502][502]; // DP 테이블 생성 및 초기값 할당
     D[1][0] = input[1][0];
 
     for(int i = 2; i < n+1; i++){
