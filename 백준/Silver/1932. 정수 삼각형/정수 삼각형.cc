f0cf2437d3867d1df869af66a05f6f77830f3832
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2023-11-09 13:53:17 +0900

    [Silver I] Title: 정수 삼각형, Time: 40 ms, Memory: 3868 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -6,24 +6,24 @@ int main(){
     int n;
     cin >> n;
 
-    int input[502][502]; // 정수 삼각형
+    int input[502][502] = {}; // 정수 삼각형
     for(int i = 1; i < n+1; i++){ // line
-        for(int j = 0; j < i; j++){ // index
+        for(int j = 1; j < i+1; j++){ // index
             cin >> input[i][j];
         }
     }
 
-    int D[502][502]; // DP 테이블 생성 및 초기값 할당
-    D[1][0] = input[1][0];
+    int D[502][502] = {}; // DP 테이블 생성 및 초기값 할당
+    D[1][1] = input[1][1];
 
     for(int i = 2; i < n+1; i++){
-        for(int j = 0; j < i; j++){
+        for(int j = 1; j < i+1; j++){
             D[i][j] = max(D[i-1][j-1], D[i-1][j]) + input[i][j];
         }
     }
 
-    int res = D[n][0];
-    for(int i = 1; i < n; i++){
+    int res = D[n][1];
+    for(int i = 2; i < n+1; i++){
         res = max(res, D[n][i]);
     }
     cout << res;
