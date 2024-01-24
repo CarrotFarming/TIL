4dd221a6359e3b59c65acabe01ad03b27d49e018
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2024-01-24 23:55:03 +0900

    Time: 1033 ms (92.61%), Space: 55.1 MB (95.04%) - LeetHub

--- LeetCode/Medium/0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ LeetCode/Medium/0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -8,10 +8,10 @@ class Solution:
 
         for i in intervals[1:]:
             if end > i[0]:
-                # 현재의 endi가 다음의 starti와 겹칠 경우 제거 카운트 증가
+                # 현재의 endi가 다음의 starti와 겹칠 경우, 제거 카운트 증가
                 remove_count += 1
             else:
-                # 겹치지 않는 경우 endi 업데이트
+                # 겹치지 않는 경우, endi 업데이트
                 end = i[1]
 
         return remove_count
