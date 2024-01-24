91323a1193b28df5b9d0ffcf61b377896463b8fb
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2024-01-24 23:25:28 +0900

    Time: 1021 ms (95.96%), Space: 55.3 MB (80.91%) - LeetHub

--- LeetCode/Medium/0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ LeetCode/Medium/0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,15 +1,18 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
 
-        intervals.sort(key=lambda x: x[1]) # endi 기준 정렬
+        # endi 기준 정렬
+        intervals.sort(key=lambda x: x[1])
         
-        overlap = 0
+        removeCount = 0 
         end = intervals[0][1]
+
         for i in range(1, len(intervals)):
-            if end <= intervals[i][0]:
-                end = intervals[i][1]
+            if end > intervals[i][0]:
+                # 현재의 endi가 다음의 starti와 겹칠 경우 제거 카운트 증가
+                removeCount += 1
             else:
-                overlap += 1
-                 
-        return overlap
-    
+                # 겹치지 않는 경우 endi 업데이트
+                end = intervals[i][1]
+
+        return removeCount
