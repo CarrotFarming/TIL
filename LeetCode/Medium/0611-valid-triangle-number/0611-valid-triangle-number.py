1969e5f6c2e144b6a0627e6e3919b493bb139c8a
Kim Jiwon <71117552+KJW988@users.noreply.github.com>
2024-02-01 05:47:04 +0900

    Time: 1207 ms (37.51%), Space: 16.7 MB (49.52%) - LeetHub

--- LeetCode/Medium/0611-valid-triangle-number/0611-valid-triangle-number.py
+++ LeetCode/Medium/0611-valid-triangle-number/0611-valid-triangle-number.py
@@ -1,3 +1,5 @@
+from bisect import bisect_left
+
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         nums.sort() # 이진탐색을 위한 정렬
@@ -9,17 +11,7 @@ class Solution:
                 continue
             for j in range(i + 1, len_nums - 1):
                 tmp = nums[i] + nums[j]
-                left, right = j + 1, len_nums - 1
-
-                # 삼각 부등식을 만족하는 변 탐색
-                while left <= right:
-                    mid = (left + right) // 2
-
-                    if tmp <= nums[mid]:
-                        right = mid - 1
-                    else:
-                        left = mid + 1
-                # j와 right 사이의 모든 값: 삼각 부등식을 만족
-                cnt += (right - j)
+                index = bisect_left(nums, tmp, j + 1)
+                cnt += (index - (j + 1))
 
         return cnt
